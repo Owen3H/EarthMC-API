670a19de5ecac651920915a31f860502dc921f3e
Owen77Stubbs <owen77stubbs@gmail.com>
2020-07-03 11:58:05 +0100

    added error handling

--- app.js
+++ app.js
@@ -5,9 +5,24 @@ const express = require("express")
       residentsRoute = require("./api/routes/residents"),
       serverInfoRoute = require("./api/routes/serverInfo")
 
+// Use the routes defined in api/routes
 app.use("/towns", townsRoute)
 app.use("/nations", nationsRoute)
 app.use("/residents", residentsRoute)
 app.use("/serverinfo", serverInfoRoute)
+
+// Error handling
+app.use((req, res, next) => 
+{
+      const error = new Error('Not found!')
+      error.status = 404
+      next(error)
+})
+
+app.use((error, req, res, next) => 
+{
+      res.status(error.status || 500)
+      res.json(error)
+})
 
 module.exports = app
