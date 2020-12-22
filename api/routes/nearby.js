9d3aeba41dec232c981af37d9c0b448bb2fb2fd8
Owen Stubbs <owen77stubbs@gmail.com>
2020-12-22 20:28:08 +0000

    update npm version for nearby fixes

--- api/routes/nearby.js
+++ api/routes/nearby.js
@@ -9,9 +9,8 @@ router.get("/:xPos/:zPos/:xBlocks/:zBlocks", async (req, res, next) =>
     if (!xBlocks) xBlocks = 500
     if (!zBlocks) zBlocks = 500
 
-    var nearbyPlayers = []
-
-    nearbyPlayers = await emc.nearTo(Number(req.params.xPos), Number(req.params.zPos), Number(xBlocks), Number(zBlocks)).then(players => { return players })
+    var nearbyPlayers = await emc.nearTo(Number(req.params.xPos), Number(req.params.zPos), Number(xBlocks), Number(zBlocks)).then(players => { return players })
+    
     if (!nearbyPlayers) res.status(200).json([])
     else res.status(200).json(nearbyPlayers)
 })
