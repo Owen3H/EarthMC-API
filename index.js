15b50960bd282e69ee6c6c834c9e2340a416e137
Owen Stubbs <owen77stubbs@gmail.com>
2022-02-22 14:30:54 +0000

    test cert installation

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const http = require("http"),
+      https = require("https"),
       app = require("./app"),
       port = process.env.PORT || 3001
-      server = http.createServer(app)
+      
+const authOptions = {
+    cert:  fs.readFileSync('./web/auth/certificate.crt'),
+    ca: fs.readFileSync('./web/auth/ca_bundle.crt'),
+    key: fs.readFileSync('./web/auth/private.key')
+ };
+ 
+const httpServer = http.createServer(app),
+      httpsServer = https.createServer(authOptions, app)
 
-server.listen(port)
+app.use((req, res, next) => 
+{
+    if (req.protocol === 'http') 
+    {
+        res.redirect(301, `https://${req.headers.host}${req.url}`);
+    }
+    
+    next();
+});
+
+httpServer.listen(port)
+httpsServer.listen(port)
