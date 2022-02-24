9d7e9d791a91ba3244fca03807ebc4f04502bb71
Owen Stubbs <owen77stubbs@gmail.com>
2022-02-24 12:10:00 +0000

    fix invite and tweak padding in overlook section

--- routes/main.js
+++ routes/main.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res)
     res.sendFile(`${process.cwd()}/web/index.html`)
 })
 
+router.get('/invite', function(req, res) 
+{
+    res.sendFile(`${process.cwd()}/web/invite.html`)
+})
+
 module.exports = router
