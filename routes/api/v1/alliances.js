46ef06ef15786c12b1b350ed63b0f9a51b46a5a7
Owen Stubbs <owen77stubbs@gmail.com>
2022-03-01 00:03:55 +0000

    1 minute timeout + unauthorized check

--- routes/api/v1/alliances.js
+++ routes/api/v1/alliances.js
@@ -12,13 +12,11 @@ router.put('/', function (req, res)
     if (req.header('AUTH_KEY') == process.env.AUTH_KEY)
     {
         var alliances = req.body
+
+        res.setTimeout(60000)
         res.status(200).json(alliances)
     }
-})
-
-router.get('/', function (req, res)
-{
-    res.status(200)
+    else res.status(404).send("PUT request unauthorized!")
 })
 
 module.exports = router;
