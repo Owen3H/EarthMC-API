2c4d6ec446b1b605beb1573dcaf107fabfa87c55
Owen Stubbs <owen77stubbs@gmail.com>
2022-03-01 01:58:14 +0000

    b

--- routes/api/v1/alliances.js
+++ routes/api/v1/alliances.js
@@ -33,7 +33,7 @@ router.put('/', function (req, res)
         cache.put('alliances', alliances, cacheTimeout)
 
         res.setTimeout(cacheTimeout)
-        res.status(200).json(alliances)
+        res.status(200).send(alliances)
     }
     else res.status(404).json("PUT request unauthorized!")
 })
