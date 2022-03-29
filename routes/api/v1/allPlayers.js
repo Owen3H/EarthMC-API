5cd8bf59565a240e7f7f3c22da8cfee0a6e5a3eb
Owen Stubbs <owen77stubbs@gmail.com>
2022-03-29 03:52:49 +0100

    merge

--- routes/api/v1/allPlayers.js
+++ routes/api/v1/allPlayers.js
@@ -6,13 +6,16 @@ const express = require("express"),
       
 var cacheTimeout = 30000
 
-router.put('/', cors(), function (req, res) 
+const mergeById = (a1, a2) => a1.map(itm => ({...a2.find((item) => (item.name === itm.name) && item), ...itm}));
+
+router.put('/', cors(), async function (req, res) 
 {
     if (req.header('AUTH_KEY') == process.env.AUTH_KEY) {
-        var players = req.body
+        var allPlayers = await emc.getAllPlayers().then(players => { return players }),
+            players = req.body
 
-        cache.put('players', players, cacheTimeout)
-        res.status(200).json(players).setTimeout(cacheTimeout)
+        cache.put('players', mergeById(allPlayers, players), cacheTimeout)
+        res.status(200).json(mergeById(allPlayers, players)).setTimeout(cacheTimeout)
     }
     else res.status(401).send("PUT request unauthorized!")
 })
