d2eccb21c8811374342410bbbd32560e0cfb53fa
Owen Stubbs <owen77stubbs@gmail.com>
2022-04-02 17:47:31 +0100

    /allianceName, /meganations and /submeganations

--- routes/api/v1/alliances.js
+++ routes/api/v1/alliances.js
@@ -15,14 +15,45 @@ router.get('/', function (req, res)
 {
     var cachedAlliances = cache.get('alliances')
 
-    if (cachedAlliances) res.status(200).json(cachedAlliances).setTimeout(timeout)
-    else res.status(202).send("Alliances are not updated yet.")
+    if (cachedAlliances) send200(cachedAlliances)
+    else send202(res)
+})
+
+router.get('/:allianceName', function (req, res) 
+{
+    var cachedAlliances = cache.get('alliances'),
+        allianceName = req.params.allianceName
+
+    if (cachedAlliances) {
+        let alliance = cachedAlliances.find(cur => cur.allianceName.toLowerCase() == allianceName.toLowerCase())
+
+        if (!alliance) res.status(404).json("That alliance does not exist!")
+        else send200(alliance)
+    }
+    else send202(res)
+})
+
+router.get('/meganations', function (req, res) 
+{
+    var cachedAlliances = cache.get('alliances'),
+        meganations = cachedAlliances.filter(alliance => alliance.type == 'mega')
+
+    if (cachedAlliances) send200(meganations)
+    else send202(res)
+})
+
+router.get('/submeganations', function (req, res) 
+{
+    var cachedAlliances = cache.get('alliances'),
+        submeganations = cachedAlliances.filter(alliance => alliance.type == 'sub')
+
+    if (cachedAlliances) send200(submeganations)
+    else send202(res)
 })
 
 router.put('/', cors(), function (req, res) 
 {
-    if (req.header('AUTH_KEY') == process.env.AUTH_KEY)
-    {
+    if (req.header('AUTH_KEY') == process.env.AUTH_KEY) {
         var alliances = req.body
 
         cache.put('alliances', alliances)
@@ -30,5 +61,13 @@ router.put('/', cors(), function (req, res)
     }
     else res.status(401).send("PUT request unauthorized!")
 })
+
+function send200(res, data) {
+    res.status(200).json(data).setTimeout(timeout)
+}
+
+function send202(res) {
+    res.status(202).send("Alliances are not updated yet.")
+}
 
 module.exports = router
