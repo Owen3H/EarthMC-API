392c55e8a10872e2f32cfbb79cd522c3d8554755
Owen Stubbs <owen77stubbs@gmail.com>
2022-04-12 22:19:40 +0100

    scout apm test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // @ts-nocheck
-const express = require("express")
+const scout = require("@scout_apm/scout-apm"),
+      express = require("express"),
       app = express(),
       rateLimit = require('express-rate-limit'),
       mainRoute = require("./routes/webpage/main"),
@@ -21,79 +22,106 @@ const express = require("express")
       alliancesRoute = require("./routes/api/v1/alliances"),
       newsRoute = require("./routes/api/v1/news")
 
-var window = 10 * 1000
-const limiter = rateLimit({
-      windowMs: window, // Time (ms) until limit is reset
-      max: 24, // Limit each IP to x requests per `window`
-      message: 'You are currently rate-limited, try again in ' + window/1000 + ' seconds.',
-      standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-      legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
-      
-app.set('trust proxy', 1)
-app.use(limiter)
-
-const compression = require('compression')
-app.use(compression()) // Compress all routes
-
-var bodyParser = require("body-parser")
-app.use(bodyParser.json({ limit: '20mb' }))
-app.use(bodyParser.urlencoded({ limit: "20mb", extended: true, parameterLimit: 20000 }))
-
-// Serve webpage routes.
-app.use("/", mainRoute)
-app.use("/invite", inviteRoute)
-//app.use("/map", mapRoute)
-
-// Serve API routes.
-app.use("/api/v1/towns", townsRoute)
-app.use("/api/v1/nations", nationsRoute)
-app.use("/api/v1/residents", residentsRoute)
-app.use("/api/v1/serverinfo", serverInfoRoute)
-app.use("/api/v1/onlineplayers", onlinePlayersRoute)
-app.use("/api/v1/townlessplayers", townlessPlayersRoute)
-app.use("/api/v1/allplayers", allPlayersRoute)
-app.use("/api/v1/nearby", nearbyPlayersRoute)
-app.use("/api/v1/nearbyplayers", nearbyPlayersRoute)
-app.use("/api/v1/nearbytowns", nearbyTownsRoute)
-app.use("/api/v1/nearbynations", nearbyNationsRoute)
-
-// Redirects
-app.use("/api/v1/online", onlineRedirect)
-app.use("/api/v1/players", playersRedirect)
-app.use("/api/v1/townless", townlessRedirect)
-
-// POST, PUT, DELETE restricted to EMC Stats.
-app.use("/api/v1/alliances", alliancesRoute)
-app.use("/api/v1/news", newsRoute)
-app.use("/api/v1/allplayers", allPlayersRoute)
-
-// Default not found response
-app.use((req, res) => 
-{
-      var date = new Date()
-
-      res.json({
-            "timestamp": date.getTime(),
-            "status": 404,
-            "error": "Endpoint Error",
-            "message": "Not found!",
-            "path": req.path
+// The "main" function
+async function start() {
+      // Trigger the download and installation of the core-agent
+      await scout.install({
+            allowShutdown: true, // allow shutting down spawned scout-agent processes from this program
+            monitor: true, // enable monitoring
+            name: "",
+            key: "",
       })
-})
 
-// Error handling
-app.use((error, req, res) => 
-{
-      var date = new Date()
+      // Initialize your express application
+      const app = express()
 
-      res.json({
-            "timestamp": date.getTime(),
-            "status": error.status || 500,
-            "error": "Internal Server Error",
-            "message": error.message,
-            "path": req.path
+      // Enable the app-wide scout middleware
+      app.use(scout.expressMiddleware())
+
+      run()
+
+      // Start express
+      app.start()
+}
+
+async function run() {
+      var window = 5 * 1000
+      const limiter = rateLimit({
+            windowMs: window, // Time (ms) until limit is reset
+            max: 10, // Limit each IP to x requests per `window`
+            message: 'You are currently rate-limited, try again in ' + window/1000 + ' seconds.',
+            standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+            legacyHeaders: false, // Disable the `X-RateLimit-*` headers
       })
-})
+            
+      app.set('trust proxy', 1)
+      app.use(limiter)
+
+      const compression = require('compression')
+      app.use(compression()) // Compress all routes
+
+      var bodyParser = require("body-parser")
+      app.use(bodyParser.json({ limit: '20mb' }))
+      app.use(bodyParser.urlencoded({ limit: "20mb", extended: true, parameterLimit: 20000 }))
+
+      // Serve webpage routes.
+      app.use("/", mainRoute)
+      app.use("/invite", inviteRoute)
+      //app.use("/map", mapRoute)
+
+      // Serve API routes.
+      app.use("/api/v1/towns", townsRoute)
+      app.use("/api/v1/nations", nationsRoute)
+      app.use("/api/v1/residents", residentsRoute)
+      app.use("/api/v1/serverinfo", serverInfoRoute)
+      app.use("/api/v1/onlineplayers", onlinePlayersRoute)
+      app.use("/api/v1/townlessplayers", townlessPlayersRoute)
+      app.use("/api/v1/allplayers", allPlayersRoute)
+      app.use("/api/v1/nearby", nearbyPlayersRoute)
+      app.use("/api/v1/nearbyplayers", nearbyPlayersRoute)
+      app.use("/api/v1/nearbytowns", nearbyTownsRoute)
+      app.use("/api/v1/nearbynations", nearbyNationsRoute)
+
+      // Redirects
+      app.use("/api/v1/online", onlineRedirect)
+      app.use("/api/v1/players", playersRedirect)
+      app.use("/api/v1/townless", townlessRedirect)
+
+      // POST, PUT, DELETE restricted to EMC Stats.
+      app.use("/api/v1/alliances", alliancesRoute)
+      app.use("/api/v1/news", newsRoute)
+      app.use("/api/v1/allplayers", allPlayersRoute)
+
+      // Default not found response
+      app.use((req, res) => 
+      {
+            var date = new Date()
+
+            res.json({
+                  "timestamp": date.getTime(),
+                  "status": 404,
+                  "error": "Endpoint Error",
+                  "message": "Not found!",
+                  "path": req.path
+            })
+      })
+
+      // Error handling
+      app.use((error, req, res) => 
+      {
+            var date = new Date()
+
+            res.json({
+                  "timestamp": date.getTime(),
+                  "status": error.status || 500,
+                  "error": "Internal Server Error",
+                  "message": error.message,
+                  "path": req.path
+            })
+      })
+}
+
+// If this script is executed directly, run the start function
+if (require.main === module) start()
 
 module.exports = app
