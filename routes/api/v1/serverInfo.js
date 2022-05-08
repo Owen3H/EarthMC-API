9ff2e3deaf7a8cf0240410e85333c5ad632403fe
Owen Stubbs <owen77stubbs@gmail.com>
2022-05-08 16:37:39 +0100

    upload ssl

--- routes/api/v1/serverInfo.js
+++ routes/api/v1/serverInfo.js
@@ -2,9 +2,8 @@ const express = require("express"),
       router = express.Router(),
       emc = require("earthmc")
 
-router.get("/", async (req, res) => 
-{
-    var serverInfo = await emc.getServerInfo().then(info => { return info }).catch(() => {})
+router.get("/", async (req, res) => {
+    var serverInfo = await emc.getServerInfo().catch(() => {})
 
     if (!serverInfo) 
         return res.status(500).json("An error occured fetching data, please try again.")
--- routes/webpage/main.js
+++ routes/webpage/main.js
@@ -1,9 +1,12 @@
 const express = require("express"),
       router = express.Router()
-
-router.get('/', function(req, res) 
-{
+      
+router.get('/', function(req, res) {
     res.sendFile(`${process.cwd()}/web/index.html`)
+})
+
+router.get('/.well-known/pki-validation/9EEA9B71D23DC79A6C31617A1961A9F3.txt', function(req, res) {
+    res.sendFile(`${process.cwd()}/web/static/ssl.txt`)
 })
 
 module.exports = router
