4700c7fee0eb24b2029bddb71d01fcf826bb1c48
Owen Stubbs <owen77stubbs@gmail.com>
2022-05-19 14:26:07 +0100

    couple more redirets to website

--- routes/web/main.js
+++ routes/web/main.js
@@ -1,12 +1,22 @@
 const express = require("express"),
       router = express.Router()
 
+//#region Routes to redirect to docs
 router.get('/', function(req, res) {
+    res.redirect('https://emc-toolkit.vercel.app/docs')
+})
+
+router.get('/api/v1', function(req, res) {
+    res.redirect('https://emc-toolkit.vercel.app/docs')
+})
+
+router.get('/api', function(req, res) {
     res.redirect('https://emc-toolkit.vercel.app/docs')
 })
 
 router.get('/invite', function(req, res) {
     res.redirect('https://emc-toolkit.vercel.app/invite')
 })
+//#endregion
 
 module.exports = router
