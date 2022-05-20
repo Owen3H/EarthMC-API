a80e0489e41f14da806a1d6f07f3fa56d594b245
Owen Stubbs <owen77stubbs@gmail.com>
2022-05-20 01:24:03 +0100

    dddddd

--- routes/api/v1/nova/allPlayers.js
+++ routes/api/v1/nova/allPlayers.js
@@ -8,10 +8,9 @@ var timeout = 15000
 
 const mergeByName = (a1, a2) => a1.map(itm => ({...a2.find((item) => (item.name === itm.name) && item), ...itm}))
 
-router.put('/', cors(), async function (req, res) 
-{
+router.put('/', cors(), async function (req, res) {
     if (req.header('AUTH_KEY') == process.env.AUTH_KEY) {
-        var allPlayers = await emc.Nova.getAllPlayers().then(players => { return players }),
+        var allPlayers = await emc.Nova.getAllPlayers().catch(console.error),
             players = req.body
 
         if (!allPlayers) return sendError(res)
@@ -24,15 +23,13 @@ router.put('/', cors(), async function (req, res)
     else res.status(401).send("PUT request unauthorized!")
 })
 
-router.get("/", async (req, res) => 
-{
+router.get("/", async (req, res) => {
     var cachedPlayers = cache.get('players')
     if (cachedPlayers) res.status(200).json(cachedPlayers)
     else res.status(204).json("Players have not been cached yet.")
 })
 
-router.get("/:playerName", async (req, res) => 
-{
+router.get("/:playerName", async (req, res) => {
     var cachedPlayers = cache.get('players'),
         playerName = req.params.playerName.toLowerCase()
 
