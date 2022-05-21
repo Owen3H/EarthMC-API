70ac97dca0ff9688fb824243134a99462e763877
Owen Stubbs <owen77stubbs@gmail.com>
2022-05-21 16:15:56 +0100

    t

--- routes/api/v1/nova/onlinePlayers.js
+++ routes/api/v1/nova/onlinePlayers.js
@@ -2,19 +2,16 @@ const express = require("express"),
       router = express.Router(),
       emc = require("earthmc")
 
-router.get("/", async (req, res) => 
-{
-    var onlinePlayers = await emc.Nova.getOnlinePlayers(true).then(players => { return players }).catch(() => {})
-
-    if (!onlinePlayers) 
-        return res.status(500).json("An error occured fetching data, please try again.")
+router.get("/", async (req, res) => {
+    var onlinePlayers = await emc.Nova.getOnlinePlayers(true).catch(() => {})
+    if (!onlinePlayers) return res.status(500).json("An error occured fetching data, please try again.")
         
     res.status(200).json(onlinePlayers).setTimeout(5000)
 })
 
-router.get("/:onlinePlayer", async (req, res) => 
-{
-    var onlinePlayer = await emc.Nova.getOnlinePlayer(req.params.onlinePlayer).then(player => { return player }).catch(() => {})
+router.get("/:playerName", async (req, res) => {
+    var onlinePlayer = await emc.Nova.getOnlinePlayer(req.params.playerName).catch(() => {})
+    if (!onlinePlayer) res.status(500).json("Error fetching player, please try again!")
 
     if (onlinePlayer.name == "INVALID_PLAYER") res.status(404).json(onlinePlayer.message)
     else res.status(200).json(onlinePlayer).setTimeout(5000)
