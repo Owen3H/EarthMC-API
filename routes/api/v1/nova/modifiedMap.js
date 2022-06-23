14aa114e2ea91a931d4c54b7bb42e5bdb61249ed
Owen Stubbs <owen77stubbs@gmail.com>
2022-06-23 19:28:37 +0100

    Update modifiedMap.js

--- routes/api/v1/nova/modifiedMap.js
+++ routes/api/v1/nova/modifiedMap.js
@@ -10,7 +10,7 @@ router.get("/", async (req, res) => {
 
     if (cachedMapData) res.status(200).json(cachedMapData)
     else {
-        var mapData = await dynmapPlus.NOVA.getModified()
+        var mapData = await fetch("https://raw.githubusercontent.com/3meraldK/earthmc-dynmap-chromium-test/main/marker_earth.json").then(d => d.json())
         if (!mapData) return sendError(res)
 
         cache.put('nova_modified', mapData, cacheTimeout)
