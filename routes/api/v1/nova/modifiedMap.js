269f75fdf0245c1b351879e33ac23637e6fe593f
Owen Stubbs <owen77stubbs@gmail.com>
2022-06-23 20:13:18 +0100

    Update modifiedMap.js

--- routes/api/v1/nova/modifiedMap.js
+++ routes/api/v1/nova/modifiedMap.js
@@ -14,10 +14,55 @@ router.get("/", async (req, res) => {
         var mapData = await endpoint.mapData("nova")
         if (!mapData) return sendError(res)
 
-        cache.put('nova_modified', mapData, cacheTimeout)
+        let modified = modify(mapData)
+        cache.put('nova_modified', modified, cacheTimeout)
         res.status(200).json(mapData)
     }
 })
+
+async function modify(data) {
+    if (!data.sets) return
+    
+    // Delete star icons.
+    delete data.sets["townyPlugin.markerset"].markers;
+    let towns = Object.values(data.sets["townyPlugin.markerset"].areas)
+
+    // Iterating through every area drawn on the map.
+    towns.forEach(town => {
+        // Some variables.
+        var townTitle = town.desc.split('<br \/>')[0]
+        townTitle = townTitle.replace(/\(Shop\)$/g, '').replaceAll(/[()]/g, '').split(' ')
+        const nation = townTitle[2].replace('</span>', '')
+        const area = calcArea(town.x, town.z, town.x.length)
+        const memberList = town.desc.split('Members <span style=\"font-weight:bold\">')[1].split('</span><br />Flags')[0]
+        const memberSize = (memberList.match(/,/g) || []).length + 1
+
+        // Removing shop areas.
+        if (town.desc.includes('(Shop)')) town.fillopacity = town.opacity = 0
+
+        // Recreating town's description.
+        town.desc = town.desc.replace('>hasUpkeep:', '>Has upkeep:')
+                             .replace('>pvp:', '>PVP allowed:')
+                             .replace('>mobs:', '>Mob spawning:')
+                             .replace('>public:', '>Public status:')
+                             .replace('>explosion:', '>Explosions:')
+                             .replace('>fire:', '>Fire spread:')
+                             .replace('>capital:', '>Is capital:')
+
+        town.desc = town.desc.replaceAll('true<', '\u2705<').replaceAll('false<', '\u26D4<')
+        town.desc = town.desc.replace('Members <span', 'Members <b>[' + memberSize + ']</b> <span')
+        town.desc = town.desc.replace('</span><br /> Members', '</span><br />Size<span style=\"font-weight:bold\"> ' + area + ' </span><br /> Members')
+
+        // Modifying esthetics of the area.
+        town.weight = 1
+        town.opacity = 0.7
+
+        if (town.color == "#3FB4FF" && town.fillcolor == "#3FB4FF") town.color = town.fillcolor = "#000000"
+        if (nation.length < 1) return town.fillcolor = town.color = '#83003F'
+    })
+
+    return towns
+}
 
 const sendError = res => res.status(500).json("Error fetching modified map data, please try again.")
 
