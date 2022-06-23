748aefa6cd3a7f0fd0ec75bbdd17a93f31bb5338
Owen Stubbs <owen77stubbs@gmail.com>
2022-06-23 19:52:24 +0100

    local test

--- routes/api/v1/nova/modifiedMap.js
+++ routes/api/v1/nova/modifiedMap.js
@@ -1,13 +1,12 @@
 const express = require("express"),
       router = express.Router(),
-      fetch = require("node-fetch")
+      testData = require("../../../../test.json")
       // dynmapPlus = require("emc-dynmap+")
 
 router.get("/", async (req, res) => {
-    var mapData = await fetch("https://raw.githubusercontent.com/3meraldK/earthmc-dynmap-chromium-test/main/marker_earth.json").then(d => d.json())
-    if (!mapData) return sendError(res)
+    if (!testData) return sendError(res)
  
-    res.status(200).json(mapData).setTimeout(10000)
+    res.status(200).json(testData).setTimeout(10000)
 })
 
 var sendError = res => res.status(500).json("Error fetching modified map data, please try again.")
