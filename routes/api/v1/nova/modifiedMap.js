b4279007ca8db68c963e3e7aa8356f2c09371edc
Owen Stubbs <owen77stubbs@gmail.com>
2022-06-23 19:40:23 +0100

    dont cache

--- routes/api/v1/nova/modifiedMap.js
+++ routes/api/v1/nova/modifiedMap.js
@@ -1,24 +1,15 @@
 const express = require("express"),
       router = express.Router(),
-      fetch = require("node-fetch"),
-      cache = require("memory-cache")
+      fetch = require("node-fetch")
       // dynmapPlus = require("emc-dynmap+")
 
-var cacheTimeout = 30000
-
 router.get("/", async (req, res) => {
-    var cachedMapData = cache.get('nova_modified')
-
-    if (cachedMapData) res.status(200).json(cachedMapData)
-    else {
-        var mapData = await fetch("https://raw.githubusercontent.com/3meraldK/earthmc-dynmap-chromium-test/main/marker_earth.json").then(d => d.json())
-        if (!mapData) return sendError(res)
-
-        cache.put('nova_modified', mapData, cacheTimeout)
-        res.status(200).json(mapData)
-    }
+    var mapData = await fetch("https://raw.githubusercontent.com/3meraldK/earthmc-dynmap-chromium-test/main/marker_earth.json").then(d => d.json())
+    if (!mapData) return sendError(res)
+ 
+    res.status(200).json(mapData).setTimeout(10000)
 })
 
-const sendError = res => res.status(500).json("Error fetching modified map data, please try again.")
+var sendError = res => res.status(500).json("Error fetching modified map data, please try again.")
 
 module.exports = router
