3f5d8e416723418a7d2103cf1dff46d6868c9e6c
Owen Stubbs <owen77stubbs@gmail.com>
2022-06-25 23:30:19 +0100

    dont need to cache, update is every ~1 sec

--- routes/api/v1/aurora/playerUpdate.js
+++ routes/api/v1/aurora/playerUpdate.js
@@ -1,19 +1,12 @@
 const express = require("express"),
       router = express.Router(),
-      cache = require("memory-cache"),
       endpoint = require("earthmc/endpoint")
 
 router.get("/", async (req, res) => {
-    let cached = cache.get('aurora_update')
+    let data = await endpoint.playerData('aurora')
+    if (!data) return sendError(res)
 
-    if (cached) res.status(200).send(cached)
-    else {
-        let data = await endpoint.playerData('aurora')
-        if (!data) return sendError(res)
-
-        cache.put('aurora_update', data, cacheTimeout)
-        res.status(200).send(data)
-    }
+    res.status(200).send(data)
 })
 
 const sendError = res => res.status(500).json("Error fetching player update, please try again.")
