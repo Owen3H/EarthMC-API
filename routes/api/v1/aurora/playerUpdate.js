5adab305290af58e1b165b62a3540bb8a5cabbdd
Owen Stubbs <owen77stubbs@gmail.com>
2022-06-26 20:39:38 +0100

    filter area updates from player data

--- routes/api/v1/aurora/playerUpdate.js
+++ routes/api/v1/aurora/playerUpdate.js
@@ -2,12 +2,15 @@ const express = require("express"),
       router = express.Router(),
       endpoint = require("earthmc/endpoint")
 
-router.get("/", async (req, res) => {
-    let data = await endpoint.playerData('aurora')
-    if (!data) return sendError(res)
+router.get("/", async (req, res) => filterUpdate(res).then(data => res.status(200).send(data)))
 
-    res.status(200).send(data)
-})
+async function filterUpdate(res) { 
+    let raw = await endpoint.playerData('aurora')
+    if (!raw) return sendError(res)
+
+    raw.updates = raw.updates?.filter(e => e.msg != "areaupdated")
+    return raw
+}
 
 const sendError = res => res.status(500).json("Error fetching player update, please try again.")
 
--- routes/api/v1/nova/playerUpdate.js
+++ routes/api/v1/nova/playerUpdate.js
@@ -2,12 +2,15 @@ const express = require("express"),
       router = express.Router(),
       endpoint = require("earthmc/endpoint")
 
-router.get("/", async (req, res) => {
-    let data = await endpoint.playerData('nova')
-    if (!data) return sendError(res)
+router.get("/", async (req, res) => filterUpdate(res).then(data => res.status(200).send(data)))
 
-    res.status(200).send(data)
-})
+async function filterUpdate(res) { 
+    let raw = await endpoint.playerData('nova')
+    if (!raw) return sendError(res)
+
+    raw.updates = raw.updates?.filter(e => e.msg != "areaupdated")
+    return raw
+}
 
 const sendError = res => res.status(500).json("Error fetching player update, please try again.")
 
