69325d7c53ff6d56a99f8c33084c93ecaafa6e29
Owen Stubbs <owen77stubbs@gmail.com>
2022-07-04 17:04:23 +0100

    fix parse error?

--- routes/api/v1/aurora/playerUpdate.js
+++ routes/api/v1/aurora/playerUpdate.js
@@ -2,13 +2,16 @@ const express = require("express"),
       router = express.Router(),
       endpoint = require("earthmc/endpoint")
 
-router.get("/", async (req, res) => filterUpdate(res).then(data => res.status(200).send(data)))
+router.get("/", async (req, res) => {
+    let data = await filterUpdate(res)
+    res.status(200).send(data)
+})
 
 async function filterUpdate(res) { 
     let raw = await endpoint.playerData('aurora')
-    if (!raw) return sendError(res)
+    if (!raw || !raw.updates) return sendError(res)
 
-    raw.updates = raw.updates?.filter(e => e.msg != "areaupdated" && e.msg != "markerupdated")
+    raw.updates = raw.updates.filter(e => e.msg != "areaupdated" && e.msg != "markerupdated")
     return raw
 }
 
