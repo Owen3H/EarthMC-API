646a1d05887bae32c8b8d3106a380c21d0e73127
Owen Stubbs <owen77stubbs@gmail.com>
2022-07-05 19:11:44 +0100

    Revert  "implement put req"

--- routes/api/v1/nova/modifiedMap.js
+++ routes/api/v1/nova/modifiedMap.js
@@ -3,7 +3,7 @@ const express = require("express"),
       cache = require("memory-cache"),
       modify = require("earthmc-dynmap-plus")
 
-var cacheTimeout = 30*1000
+var cacheTimeout = 15000
 
 async function sendModified(cacheKey, res) {
     let cachedMapData = cache.get(cacheKey),
@@ -18,17 +18,6 @@ async function sendModified(cacheKey, res) {
         res.status(200).send(modified)
     }
 }
-
-async function putModified(cacheKey, res) {
-    if (req.header('AUTH_KEY') == process.env.EDP_AUTH_KEY)
-        return res.status(401).send("PUT request unauthorized!")
-
-    cache.put(cacheKey, req.body, cacheTimeout)
-    res.status(200).send(req.body)
-}
-
-router.put("/mega", async (req, res) => putModified('nova_mega', res))
-router.put("/pact", async (req, res) => putModified('nova_normal', res))
 
 router.get("/mega", async (req, res) => sendModified('nova_mega', res))
 router.get("/pact", async (req, res) => sendModified('nova_normal', res))
